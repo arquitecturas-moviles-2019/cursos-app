b6c516c4eff78c8aac706aaf4518f2df7d3ec0b5
Fonsho1997 <davidealonso.da@gmail.com>
2019-05-01 19:36:24 -0300

    añadido logica algoritmo curso
    Took 50 minutes

---
+++ app/src/main/java/com/arquitecturasmoviles/asado/AdaptCurseListActivity.java
@@ -0,0 +1,60 @@
+package com.arquitecturasmoviles.asado;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.Curso;
+
+import java.util.ArrayList;
+
+public class AdaptCurseListActivity extends BaseAdapter {
+    Context context;
+    ArrayList<Curso>listadoCursosDelEvento;
+    String lugar;
+
+    public AdaptCurseListActivity(ArrayList<Curso>listadoCursosDelEvento, Context context, String lugar){
+        this.listadoCursosDelEvento = listadoCursosDelEvento;
+        this.context = context;
+        this.lugar = lugar;
+    }
+
+
+    @Override
+    public int getCount() {
+        return listadoCursosDelEvento.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listadoCursosDelEvento.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).
+                    inflate(R.layout.adapt_curse_list_activity, parent, false);
+        }
+
+        Curso currentCurse = (Curso) getItem(position);
+
+        TextView tv_nombreCurso = convertView.findViewById(R.id.tv_tituloAdaptadorCurso);
+        TextView tv_fecha = convertView.findViewById(R.id.tv_AdaptadorCursoFechaHora);
+        TextView tv_lugar = convertView.findViewById(R.id.tv_adaptadorCursoLugar);
+
+        tv_nombreCurso.setText(currentCurse.getNombre());
+        tv_fecha.setText(currentCurse.getDiaHora());
+        tv_lugar.setText(this.lugar);
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/arquitecturasmoviles/asado/CursosActivity.java
@@ -0,0 +1,66 @@
+package com.arquitecturasmoviles.asado;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+
+import com.example.Curso;
+import com.example.Evento;
+
+import java.util.ArrayList;
+
+public class CursosActivity extends AppCompatActivity {
+
+    ListView listadoDondeSeVisualiza;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_cursos);
+
+        listadoDondeSeVisualiza = findViewById(R.id.lv_cursos);
+
+        Intent intencion = getIntent();
+        Bundle extras = intencion.getExtras();
+        Evento evento = (Evento)extras.get("Evento");
+
+        cargarCursosDelEvento(evento);
+    }
+
+    private void cargarCursosDelEvento(Evento evento){
+        ArrayList<Curso>listadoCursosDelEvento = new ArrayList<>();
+
+        String ubicacion = evento.getLugar();
+
+        //Sentecia para obtener los cursos
+        //Por ahora hardcodeo - Inicio hardcodeo
+        Curso curso1 = new Curso();
+        curso1.setNombre("curso1");
+        curso1.setDiaHora("1/5/19");
+        Curso curso2 = new Curso();
+        curso2.setNombre("curso2");
+        curso2.setDiaHora("2/6/19");
+        Curso curso3 = new Curso();
+        curso3.setNombre("curso3");
+        curso3.setDiaHora("3/7/19");
+
+        listadoCursosDelEvento.add(curso1);
+        listadoCursosDelEvento.add(curso2);
+        listadoCursosDelEvento.add(curso3);
+        //Fin hardcodeo
+
+        AdaptCurseListActivity adaptador = new AdaptCurseListActivity(listadoCursosDelEvento, getApplicationContext());
+        listadoDondeSeVisualiza.setAdapter(adaptador);
+
+        listadoDondeSeVisualiza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aca se construye la intencion para ir al curso seleccionado
+            }
+        });
+
+    }
+}
