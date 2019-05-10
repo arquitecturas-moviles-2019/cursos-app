3c3ce735c47b04c72405820bc6d9b002fb996a5f
AleMinacori <alenemo4@hotmail.com>
2019-05-10 17:51:25 -0300

    added buton registrar in loginActivity

--- app/src/main/java/com/arquitecturasmoviles/asado/LoginActivity.java
+++ app/src/main/java/com/arquitecturasmoviles/asado/LoginActivity.java
@@ -129,6 +129,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 attemptLogin();
             }
         });
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_register);
+            }
+        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
