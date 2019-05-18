5f35de6a4c06c279adee14ced75e6799aa1cef8d
Carlos Carrara <carlitos.carrara@hotmail.com>
2019-05-18 01:00:11 -0300

    My Courses By static ID

--- app/src/main/java/com/arquitecturasmoviles/asado/model/UserInscriptionsResponse.java
+++ app/src/main/java/com/arquitecturasmoviles/asado/model/UserInscriptionsResponse.java
@@ -1,4 +1,33 @@
 package com.arquitecturasmoviles.asado.model;
 
+import java.util.ArrayList;
+
 public class UserInscriptionsResponse {
+    private String error;
+    private String mensaje;
+    private ArrayList<CursosIdResponse> cursos;
+
+    public ArrayList<CursosIdResponse> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(ArrayList<CursosIdResponse> cursos) {
+        this.cursos = cursos;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
